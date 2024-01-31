d97eac7b62298b7467bfe32e422ac427499a7700
fordes <52121395+fordes123@users.noreply.github.com>
2024-01-31 15:27:00 +0800

    chore: 🔨 Change geoip lists source

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-wget https://gcore.jsdelivr.net/gh/IceCodeNew/4Share@master/geoip_china/china_ip_list.txt -O /etc/mosdns/geoip_cn.txt > /dev/null 2>&1
-wget https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/reject-list.txt -O /etc/mosdns/geosite_category-ads-all.txt > /dev/null 2>&1
-wget https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/proxy-list.txt -O /etc/mosdns/geosite_geolocation-!cn.txt > /dev/null 2>&1
-wget https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/direct-list.txt -O /etc/mosdns/geosite_cn.txt > /dev/null 2>&1
+wget https://cdn.jsdelivr.net/gh/IceCodeNew/4Share@master/geoip_china/china_ip_list.txt -O /etc/mosdns/geoip_cn.txt > /dev/null 2>&1
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/reject-list.txt -O /etc/mosdns/geosite_category-ads-all.txt > /dev/null 2>&1
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/proxy-list.txt -O /etc/mosdns/geosite_geolocation-!cn.txt > /dev/null 2>&1
+wget https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/direct-list.txt -O /etc/mosdns/geosite_cn.txt > /dev/null 2>&1
 echo "Update geoip rule completed."
 
 # mosdns
